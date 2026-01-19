e6ca557b5f66d5cf2fff2c354c80d9258d02094b
Ritsu- <biasonneilaldous@gmail.com>
2026-01-19 08:42:54 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="text-center p-6 mt-16 text-gray-600 dark:text-gray-400 text-sm border-t dark:border-gray-700">
-      &copy; 2025 React Hobby Site. By L2Dev Group 4 ✨
+      &copy; 2025 React Hobby Site. All rights reserved.
     </footer>
   );
 }
